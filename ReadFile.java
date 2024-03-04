9a266fe806ffcd088b67186b981e259a6d16bac9
buddy <kluong@smith.edu>
2024-03-04 11:24:26 -0500

    added ReadFile class

---
+++ ReadFile.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
+/** Class containing a file reading demo */
+public class ReadFile {
+    /** The main method reads from a file and prints the contents. */
+    public static void main(String[] args) {
+        // Figuring out input source
+        Scanner file = null;
+        if (args.length >0) {
+            String filename = args[0];
+            try {
+                file = new Scanner(new File(filename));
+            } catch (FileNotFoundException e) {
+                System.err.println("Cannot locate file.");
+                System.exit(-1);
+            }
+        } else {
+            file = new Scanner(System.in);
+        }
+
+        // reading from input
+        while (file.hasNextLine()) {
+            String line = file.nextLine();
+            System.out.println("Happy birthday, " + line);
+        }
+        file.close();
+    }
+} 
